be93acc206e40189e1708280eb4ebd66d24ee7ba
Nantaphop Phuengphae <nantaphop.p@gmail.com>
2016-11-25 21:23:05 +0700

    asd

--- bot.php
+++ bot.php
@@ -15,11 +15,15 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+            $replyText = $text;
+            if($text == 'พ่อวินชื่อไร'){
+                $replyText = 'เจริญชัยไงครับ';
+            }
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $replyText
 			];
 
 			// Make a POST Request to Messaging API to reply to sender

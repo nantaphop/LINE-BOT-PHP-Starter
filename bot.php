b2521e8cc170170e4d22cc217ff78119c2130869
Nantaphop Phuengphae <nantaphop.p@gmail.com>
2016-11-25 21:16:57 +0700

    Add Auth

--- bot.php
+++ bot.php
@@ -1,2 +1,15 @@
 <?php
-echo "I am a bot";
+$access_token = 'milcol1eihnEjRBq1K2YsDxZePWnLOcC6fvz38MMWqrA9C/LAYbYlV15Pq0NQS3dy/DLRx+XiH3K7Xb5mr44+ej8pBVVAmpxHiG37VPK+DlUBVwYePDgWzIdS/fHul9mSD3npXl7Xk+c+MTS690lDAdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
